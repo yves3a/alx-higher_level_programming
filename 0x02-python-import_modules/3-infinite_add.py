13a3b8cc482e6e41b1b2c1ef325d778674c09f0c
yves3a <yves.irakoz3@gmail.com>
2024-09-20 17:53:32 +0200

    Infite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    result = 0
+
+    # loop through each argument and convert it into an interger.
+    for i in argv:
+        result += int(i)
+
+    #print the final result
+    print("{}".format(result))
