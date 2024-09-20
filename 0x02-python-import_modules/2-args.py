f76a32a5b06f59573cd127c88d4357265264d6cb
yves3a <yves.irakoz3@gmail.com>
2024-09-20 15:39:22 +0200

    Listing arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:] # list of arg excluding script name
+    a = len(argv)
+
+    if a == 0:
+        print("{} arguments.".format(a))
+    elif a == 1:
+        print("{} argument:".format(a))
+    else:
+        print("{} arguments:".format(a))
+
+    for i in range(1, a + 1):
+            print("{}: {}".format(i, sys.argv[i]))
