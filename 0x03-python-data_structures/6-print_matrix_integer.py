e2a41e6e57f232d00447cc14b1afc409d5338f4d
yves3a <yves.irakoz3@gmail.com>
2024-09-27 01:05:54 +0200

    Fixing a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for row in matrix:
+    for row in matrix:
+        if len(row) == 0:
+            print()  # Print a blank line for empty row
+        else:
             for i in range(len(row)):
                 if i != len(row) - 1:
                     print("{:d}".format(row[i]), end=" ")
                 else:
                     print("{:d}".format(row[i]), end="")
-                    print()
+            print()  # Move to the next line after each row
+
