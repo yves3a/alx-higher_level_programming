37497b8f21e5f0728a0fb6ac5b3195992b19b78b
yves3a <yves.irakoz3@gmail.com>
2024-09-27 00:57:50 +0200

    fixing a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
             if i != len(row) - 1:
-                print("{}".format(row[i]), end=", ")
+                print("{}".format(row[i]), end=" ")
             else:
-                print("{}".format(row[i]), end=", ")
+                print("{}".format(row[i]), end="")
                 print()
