d6380a0d64fdc2ebf4d070315080e3c5fe3930b0
yves3a <yves.irakoz3@gmail.com>
2024-09-27 11:46:37 +0200

    Only multiples of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     new_list = []
 
-    for i in my_list:
-        if i % 2 == 0:
+    for num in my_list:
+        if num % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
