e80197111f122825cb43964b3a0e20521b4876fa
yves3a <yves.irakoz3@gmail.com>
2024-09-27 11:57:05 +0200

    Deleting at a cetain position in the list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+
+    if idx < 0:
+        return my_list
+
+    elif idx not in range(len(my_list)):
+        return my_list
+
+    else:
+        # Deleting element at idx position
+        del my_list[idx]
+        return my_list
