27d61c544f3564a15bb5f6772be3db9370de42ce
yves3a <yves.irakoz3@gmail.com>
2024-10-02 16:27:02 +0200

    Squaring a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    
+
     new_matrix = []
-    
+
     # iterating through each row
     for row in matrix:
+        # creating a new row
         new_row = []
         for i in row:
             new_row.append(i**2)
