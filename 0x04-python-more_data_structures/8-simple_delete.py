51f5407f08c60799705902f8e2a79d5e82490bec
yves3a <yves.irakoz3@gmail.com>
2024-10-02 22:29:44 +0200

    Simple key delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for memb in a_dictionary:
-        if memb == key:
-            del a_dictionary[key]
-        return a_dictionary
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
