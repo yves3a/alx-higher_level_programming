74039ca979860fcdfcc125826e3b7bba8613c320
yves3a <yves.irakoz3@gmail.com>
2024-10-03 13:18:57 +0200

    Complex deleting

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
+
 def complex_delete(a_dictionary, value):
+    """
+    Delete keys with a specific value from a dictionary.
 
-    # copying a dictionary
-    new_dic = a_dictionary.copy()
+    Args:
+        a_dictionary (dict): The input dictionary.
+        value: The value to search for and delete in the dictionary.
 
-    # Identifying keys to delete
-    keys_to_delete = [key for key in a_dictionary if a_dictionary[key] == value]
+    Returns:
+        dict: The modified dictionary after deleting the keys.
 
-    # iterate within keys to  delete while deleting
-    for memb in keys_to_delete:
-        del new_dic[memb]
-    return new_dic
+    """
+    # Create a list to store the keys to delete
+    keys_to_delete = []
+
+    # Iterate over each key in the dictionary
+    for key in a_dictionary:
+        # Check if the value of the current key matches the given value
+        if a_dictionary[key] == value:
+            # Add the key to the list of keys to delete
+            keys_to_delete.append(key)
+
+    # Delete the keys from the dictionary using the pop() method
+    for key in keys_to_delete:
+        a_dictionary.pop(key, None)
+
+    return a_dictionary
