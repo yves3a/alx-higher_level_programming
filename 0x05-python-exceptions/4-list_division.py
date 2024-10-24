b7a13114bb07502bf457a01873116f9915c432ab
yves3a <yves.irakoz3@gmail.com>
2024-10-24 17:56:54 +0200

    Divide list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,16 +11,16 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             division_result = 0
             print("out of range")
-        
+
         except TypeError:
             division_result = 0
             print("wrong type")
-        
+
         except ZeroDivisionError:
             division_result = 0
             print("division by 0")
 
-        finally: 
+        finally:
             result.append(division_result)
 
     return result
