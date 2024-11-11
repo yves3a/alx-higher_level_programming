29d59f7620c0682eaf188fc0d2b51318261602ee
yves3a <yves.irakoz3@gmail.com>
2024-11-11 15:56:07 +0200

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -15,10 +15,22 @@ def text_indentation(text):
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    
+    i = 0
+    while i < len(text):
+        # Skip spaces at the beginning of the text
+        if text[i] == " " and i == 0:
+            i += 1
+            continue
+        # Skip spaces after . ? and :
+        if i > 0 and text[i] == " " and text[i - 1] in ".?:":
+            i += 1
+            continue
+        
+        #
+        print(text[i], end="")
 
-    # Print the text with 2 new lines after . ? and :
-    for char in text:
-        if char in ".?:":
+        # Print 2 new lines after . ? and : 
+        if text[i] in ".?:":
             print("\n")
-        else:
-            print(char, end="")
+        i += 1
