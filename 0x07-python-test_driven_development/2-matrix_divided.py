e4d38e38d08b1f2bc20b8e0a6ab52a2013d04671
yves3a <yves.irakoz3@gmail.com>
2024-11-11 11:04:33 +0200

    Divide matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,31 +3,18 @@
 
 
 def matrix_divided(matrix, div):
-    """Divides all elements of a matrix by div.
-
-    Args:
-        matrix: List of lists containing integers or floats
-        div: Number to divide matrix elements by
-
-    Returns:
-        New matrix with elements divided by div
-
-    Raises:
-        TypeError: If matrix is not list of lists of integers/floats,
-                  If rows are not same size,
-                  If div is not a number
-        ZeroDivisionError: If div is zero
-    """
-    if not isinstance(matrix, list) or not all(
-            isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    """Divides all elements of a matrix by div."""
+    msg = ("matrix must be a matrix (list of lists) "
+           "of integers/floats")
+    if not isinstance(matrix, list) or \
+       not all(isinstance(row, list) for row in matrix):
+        raise TypeError(msg)
 
     if len(matrix) == 0 or any(len(row) == 0 for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(msg)
 
-    if not all(isinstance(element, (int, float))
-              for row in matrix for element in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(x, (int, float)) for row in matrix for x in row):
+        raise TypeError(msg)
 
     row_size = len(matrix[0])
     if not all(len(row) == row_size for row in matrix):
@@ -39,4 +26,4 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    return [[round(element / div, 2) for element in row] for row in matrix]
+    return [[round(x / div, 2) for x in row] for row in matrix]
