ffaa5fb7420153fd82e087bc7140d28e8d3a1eae
yves3a <yves.irakoz3@gmail.com>
2024-12-31 13:05:41 +0200

    Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,55 +1,26 @@
 #!/usr/bin/python3
+
 """
-Represents class that inherits from BaseGeometry class
+A module that inherits from the BaseGeometry class and models a
+Rectangle object
 """
 
-
-class BaseGeometry:
-    """
-    A class raises an exception
-    """
-
-    def area(self):
-        """
-        Define a mothod area
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Define integer_validator
-
-
-        Args:
-        name(str): The name
-        value(int): The integer to be valiadated.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if (value <= 0):
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    A class that inherits from class BaseGeometry
-    """
+    """A class that models a Rectangle."""
 
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int) -> None:
         """
-        Initializes a rectangle with width and height
+        Initializes an instance of a Rectangle object.
 
         Args:
             width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-
-        Both are validated as positive integers.
+            height (int): the width of the rectangle.
         """
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
         self.__width = width
-        super().integer_validator("height", height)
         self.__height = height
