0fcbcf7954469fdf95dbaf7b73c9d15c1b4de13e
yves3a <yves.irakoz3@gmail.com>
2025-01-29 13:31:32 +0200

    Updates

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+
 def read_file(filename=""):
     """ A funtion that reads text file and prints it to stdout """
 
-    with open(filename, encoding="utf-8") as myfile:
-        print(myfile.read(), end="")
+    with open(filename, mode = "r", encoding="utf-8") as myfile:
+        for line in myfile:
+            print(line, end="")
+    print()
