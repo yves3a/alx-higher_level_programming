8d14d73f7f4e35261adc6c687c65da740a80956f
yves3a <yves.irakoz3@gmail.com>
2025-02-04 23:14:03 +0200

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for Student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.firstname = first_name
+        self.lastname = last_name
+        self.age = age
+
+    def to_json(self):
+        """Dictionary representation of class student"""
+        return self.__dict__
